ec38d764eeb4d3099c8bfb7d5e0d42da69251a5d
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2025-05-28 22:22:01 -0500

    Moved stuff

--- Test Maps/map_01.gd
+++ Test Maps/map_01.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var character = preload("res://Character/characterController.tscn")
+@onready var character = preload("res://Test Char/TheCharacter.tscn")
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
@@ -9,12 +9,12 @@ func _input(_event: InputEvent) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
-		var old_char = $CharacterController
+		var old_char = $TheCharacter
 		old_char.name = "OldChar"
 		var new_char = character.instantiate()
 		add_child(new_char, true)
 		new_char.global_position.y = 1.0
-		new_char.name = "CharacterController"
-		old_char.get_node("WorldModel").queue_free()
-		old_char.get_node("AnimationUtils").queue_free()
+		new_char.name = "TheCharacter"
+		old_char.get_node("CharacterController/WorldModel").queue_free()
+		old_char.get_node("CharacterController/AnimationUtils").queue_free()
 		old_char.queue_free()
